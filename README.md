6a77da5a85ecad1e331b37bc12ca8ab51865008c
cjvanlissa <c.j.vanlissa@uu.nl>
2020-12-18 18:26:11 +0100

    read data

--- README.md
+++ README.md
@@ -75,6 +75,23 @@ Some researchers might want to share their work only once the paper is accepted 
 ## Access to data
 
 Some of the data used in this project are not publically available.
+Synthetic data with similar characteristics to the original data have been provided. Using the function load_data() will load these synthetic data when the original data are unavailable. Note that these synthetic data cannot be used to reproduce the original results. However, it does allow users to run the code and, optionally, generate valid code that can be evaluated using the original data by the project authors.
+To request access to the original data, [open a GitHub issue](https://docs.github.com/en/free-pro-team@latest/github/managing-your-work-on-github/creating-an-issue).
+
+<!--Clarify here how users should contact you to gain access to the data, or to submit syntax for evaluation on the original data.-->
+
+
+## Access to data
+
+Some of the data used in this project are not publically available.
+To request access to the original data, [open a GitHub issue](https://docs.github.com/en/free-pro-team@latest/github/managing-your-work-on-github/creating-an-issue).
+
+<!--Clarify here how users should contact you to gain access to the data, or to submit syntax for evaluation on the original data.-->
+
+
+## Access to data
+
+Some of the data used in this project are not publically available.
 To request access to the original data, [open a GitHub issue](https://docs.github.com/en/free-pro-team@latest/github/managing-your-work-on-github/creating-an-issue).
 
 <!--Clarify here how users should contact you to gain access to the data, or to submit syntax for evaluation on the original data.-->
--- prepare_data.R
+++ prepare_data.R
@@ -8,35 +8,38 @@ library(worcs)
 # Load data
 
 # replace this with code to load data
-source("fake_data.R")
+df <- read.csv("MARP_data.csv", stringsAsFactors = FALSE)
 # Recode missing data
 
+# NOTE: Unlike indicated in the data documentation, none of the missing
+# values described below are present in the data.
 # Missing Values 
 # 
 # 
 # The dataset contains missing values for the following variables: 
 #   ● wb_soc_3 995 missing (did not want to disclose) 
-df$wb_soc_3[which(df$wb_soc_3 == 995)] <- NA
-# ● age 27 missing 
-df$age[which(df$age == 27)] <- NA
-# ● ses 5 missing 
-df$ses[which(df$ses == 5)] <- NA
-# ● ethnicity 409 missing (in France, this question was not asked due to legal 
-#                          restrictions; no missing values in other countries)  
-df$ethnicity[which(df$ethnicity == 409)] <- NA
-# ● denomination 5676 missing (this was only asked when participants indicated to 
-#                              belong to a religious denomination [rel_4]) 
-df$denomination[which(df$denomination == 5676)] <- NA
-# There are no missing data for the remaining variables.  
+# df$wb_soc_3[which(df$wb_soc_3 == 995)] <- NA
+# # ● age 27 missing 
+# df$age[which(df$age == 27)] <- NA
+# # ● ses 5 missing 
+# df$ses[which(df$ses == 5)] <- NA
+# # ● ethnicity 409 missing (in France, this question was not asked due to legal 
+# #                          restrictions; no missing values in other countries)  
+# df$ethnicity[which(df$ethnicity == 409)] <- NA
+# # ● denomination 5676 missing (this was only asked when participants indicated to 
+# #                              belong to a religious denomination [rel_4]) 
+# df$denomination[which(df$denomination == 5676)] <- NA
+# # There are no missing data for the remaining variables.  
 
 
 # Not religious and atheist same for present purposes
-df$rel_3[which(df$rel_3 %in% c(2, 3))] <- 0
+df$rel_3 <- df$rel_3 + 1
+df$rel_3[which(df$rel_3 %in% c(1.5, 2))] <- 0
 # Recode to dummy
 df$rel_4[which(df$rel_4 %in% c(2))] <- 0
 
 # Set gender == other to NA, because number is likely to low to use as additional moderator
-df$gender[which(df$gender == 3)] <- NA
+df$gender[which(df$gender == "other")] <- NA
 
 # For each country, determine the majority ethnicity
 majority <- tapply(df$ethnicity, df$country, function(x){
@@ -47,7 +50,23 @@ majority <- tapply(df$ethnicity, df$country, function(x){
 df$majority <- as.numeric(df$ethnicity == majority[df$country])
 
 # Check data for outliers (out of range as specified in data documentation)
-worcs::descriptives(df)
+desc <- worcs::descriptives(df)
+write.csv(desc, "descriptives.csv", row.names = FALSE)
 
 # Save data
-closed_data(df)
+imp_fun <- function(x){
+  if(is.data.frame(x)){
+    return(data.frame(sapply(x, imp_fun)))
+  } else {
+    out <- x
+    if(inherits(x, "numeric")){
+      out[is.na(out)] <- median(x[!is.na(out)])
+    } else {
+      out[is.na(out)] <- names(sort(table(out), decreasing = TRUE))[1]
+    }
+    out
+  }
+}
+closed_data(df, model_expression = NULL,
+            predict_expression = sample(y, size = length(y), replace = TRUE),
+            missingness_expression = imp_fun(data))
